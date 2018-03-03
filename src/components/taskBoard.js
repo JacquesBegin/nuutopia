20afee4421bebc7eb3d8ed8f791751f4397ca7e5
raphaelz <raphaelz@users.noreply.github.com>
2018-03-03 18:35:41 -0500

    Add card examples to job board

--- src/components/taskBoard.js
+++ src/components/taskBoard.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Header from './Header.js';
+import Card from "antd/lib/card";
 
 class TaskBoard extends Component {
   render() {
-    return (
-      <div className="taskboardSection">
-        <Header/>
-        Task Board
-        Task Board
-        Task Board
-        Task Board
-        Task Board
-      </div>
-    );
+    return <div className="taskboardSection">
+        <Header />
+        <Card title="Card title" style={{ width: 300 }}>
+          <p>Card content</p>
+          <p>Card content</p>
+          <p>Card content</p>
+        </Card>
+        <Card title="Card title" style={{ width: 300 }}>
+          <p>Card content</p>
+          <p>Card content</p>
+          <p>Card content</p>
+        </Card>
+        <Card title="Card title" style={{ width: 300 }}>
+          <p>Card content</p>
+          <p>Card content</p>
+          <p>Card content</p>
+        </Card>
+        <Card title="Card title" style={{ width: 300 }}>
+          <p>Card content</p>
+          <p>Card content</p>
+          <p>Card content</p>
+        </Card>
+        <Card title="Card title" style={{ width: 300 }}>
+          <p>Card content</p>
+          <p>Card content</p>
+          <p>Card content</p>
+        </Card>
+      </div>;
   }
 }
 
