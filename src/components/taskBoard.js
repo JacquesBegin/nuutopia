9a244e3ad33644bbe25e54a493b340126a5dbee9
raphaelz <raphaelz@users.noreply.github.com>
2018-03-03 19:40:29 -0500

    Add blockchain mining calls to accept job function

--- src/components/taskBoard.js
+++ src/components/taskBoard.js
@@ -14,7 +14,42 @@ class TaskBoard extends Component {
     
     message.info("Job has been accepted!");
 
-    // TODO: Make a call to blockchain
+// Initiate transaction
+
+let walletId =
+  "c4f7818c2670da13f8e8651cbd8821b5cd5508259b080366e03865d231456ad4";
+let amount = Math.ceil(Math.random() * 10);
+let communityAddress =
+  "ef7cc1f30c1515a1306cdd93b63e3d1f7890fe26d55db7458f55ce4c5dbd9d66";
+let user1 = "50c2c488233dee2356e8da6b89709aacc30d3423808d156003c92375e8d53457";
+let user2 = "23ba997d47943fd02ca7c77d9895c4c67985884410e05af076ab361dfdadd2fa";
+
+fetch(
+  "https://missionhack-coin.herokuapp.com/operator/wallets/" + walletId + "/transactions",
+  {
+    body:
+      '{  "fromAddress": "' + communityAddress + '", "toAddress": "' + user2+ '", "amount": ' + amount + ', "changeAddress": "' + communityAddress + '" }',
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+      "password": "dead camp team build solution"
+    },
+    method: "POST"
+  }
+).then(function() {
+  
+// Finalize transaction (mine)
+    fetch("https://missionhack-coin.herokuapp.com/miner/mine", {
+      body:
+        '{ "rewardAddress": "ef7cc1f30c1515a1306cdd93b63e3d1f7890fe26d55db7458f55ce4c5dbd9d66" }',
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      method: "POST"
+    });
+});
+
     document.querySelector('.job-accepted').play();
   }  
 
